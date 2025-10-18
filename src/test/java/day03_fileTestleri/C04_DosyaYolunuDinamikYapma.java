61efea3bce4be6106881583da4f35d6be14c071f
bulutluoz <abbulutluoz@gmail.com>
2025-10-18 20:41:59 +0200

    3.ders sonu

---
+++ src/test/java/day03_fileTestleri/C04_DosyaYolunuDinamikYapma.java
@@ -0,0 +1,40 @@
+package day03_fileTestleri;
+
+import org.junit.jupiter.api.Test;
+
+public class C04_DosyaYolunuDinamikYapma {
+
+    @Test
+    public void test01(){
+        /*
+            Onceki class'larda
+            iki konumda bulunan dosyalarla ilgili testler yaptik
+            1- projenin altinda
+                /Users/ahmetbulutluoz/Desktop/MyDesktop/course/projeler/Team166_JunitFramework/src/test/java/day03_fileTestleri/deneme.txt
+            2- downloads altinda
+                /Users/ahmetbulutluoz/Downloads/logo.jpeg
+
+            Bu dosya yollari kisiden kisiye, bilgisayardan bilgisayara degisir
+
+            bu durumda yazdigimiz test sadece yazildigi bilgisayarda calisir
+            baska hic bir bilgisayarda direkt calismaz
+            MUTLAKA dosya yolunu duzeltmek gerekir.
+
+            Kodun her bilgisayar icin calismasi istenirse
+            Dosya yolu DINAMIK HALE getirilmelidir.
+
+            Java bu konuda da bize yardimci olur
+
+         */
+
+
+
+
+
+
+
+
+
+
+    }
+}
